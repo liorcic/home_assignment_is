e5ff21d3fb1c18051c76df1e85de671be0995699
Lior Cicurel <licicurel@gmail.com>
2023-03-28 21:38:09 +0300

    Addded CORS for web support

--- customer_facing_web_server/src/main.py
+++ customer_facing_web_server/src/main.py
@@ -1,3 +1,4 @@
+from fastapi.middleware.cors import CORSMiddleware
 from schemas import UserPurchaseInsert
 from utils import generate_random_buy
 from kafka import KafkaProducer
@@ -8,6 +9,19 @@ import uvicorn
 import json
 
 app = FastAPI()
+
+# Accepts requests from the front end react component
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 producer = KafkaProducer(bootstrap_servers=Config.Kafka.SERVERS,
                          value_serializer=lambda x: json.dumps(x, default=str).encode('utf-8'),
@@ -25,8 +39,12 @@ def get_buy_list(user_id: int):
     result = requests.get(f"{Config.ManagingAPI.URL}/buyList/{user_id}")
     return result.json()
 
+@app.get("/buyList")
+def get_buy_list():
+    result = requests.get(f"{Config.ManagingAPI.URL}/buyList")
+    return result.json()
 
-@app.post("/produce", response_model=UserPurchaseInsert, status_code=201)
+@app.post("/buy", response_model=UserPurchaseInsert, status_code=201)
 def produce_to_kafka():
     purchase_obj = generate_random_buy()
     print(f"Sending {purchase_obj}")
